6218364cc67ff1c5a9ae080869fc579b0c7a911e
Jack Bird <Jbird43@uwo.ca>
2022-11-29 01:13:29 -0500

    Bug fix with token varification

--- routes/VerifyToken.js
+++ routes/VerifyToken.js
@@ -5,18 +5,24 @@ module.exports = function (req, res, next) {
 
     const token = req.header('Cookie');
 
-    let [key, value] = token.split(';');
-    let[key2, value2] = key.split('=');
+    if (token === undefined) {
+        return res.status(400).send("Invalid Token");
+    } else {
+        console.log(token);
+        let [key, value] = token.split(';');
+        let [key2, value2] = key.split('=');
 
-    console.log(value2.trim());
-    console.log(value.trim());
-    if (!token) return res.status(401).send('access denied');
+        console.log(value2.trim());
+        console.log(value.trim());
+        if (!token) return res.status(401).send('access denied');
 
-    try {
-        const varified = jwt.verify(value2.trim(), "shhhhhhh");
-        req.user = varified;
-        next();
-    } catch (err) {
-        res.status(400).send("Invalid Token");
+        try {
+            const varified = jwt.verify(value2.trim(), "shhhhhhh");
+            req.user = varified;
+            next();
+        } catch (err) {
+            res.status(400).send("Invalid Token");
+        }
+
     }
 }
